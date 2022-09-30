17d1806fcc378ba77378fbe7a81b14754006dc25
SPYDR <spydr.xyz@gmail.com>
2022-09-30 15:12:40 +0600

    Added Project and app

---
+++ DJango_Prac/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DJango_Prac project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJango_Prac.settings')
+
+application = get_wsgi_application()
