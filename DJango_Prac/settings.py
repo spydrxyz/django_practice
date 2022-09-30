c2d41c8cfd75a7997e6d7cf9ab847fecceb87364
SPYDR <spydr.xyz@gmail.com>
2022-10-01 00:18:33 +0600

    Added result views and urls

--- DJango_Prac/settings.py
+++ DJango_Prac/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+DEVELOPED_APPS =[
+    'dj_app.apps.DjAppConfig'
+]
+
+INSTALLED_APPS = DEFAULT_APPS + DEVELOPED_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- dj_app/models.py
+++ dj_app/models.py
@@ -1,3 +1,23 @@
+from datetime import datetime
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=255)
+    publish_date = models.DateTimeField('Date Published')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=255)
+    vote = models.IntegerField(default='0')
+
+    def __str__(self):
+        return self.choice_text
